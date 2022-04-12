a6309120d21a4b4775618e36f0a0ad54b893c1bc
Fogha <collinslord50@gmail.com>
2022-04-12 13:42:33 +0100

    Updates

---
+++ precache-manifest.75686c34df024abbf11f3fc3e0cea948.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad56f92f6efeeddd68125a56fe7533c5",
+    "url": "/react-chess/index.html"
+  },
+  {
+    "revision": "a3109b8b170613c2b150",
+    "url": "/react-chess/static/css/main.89e11401.chunk.css"
+  },
+  {
+    "revision": "6bf7b46e11374216e899",
+    "url": "/react-chess/static/js/2.c4f5bc72.chunk.js"
+  },
+  {
+    "revision": "d2b2c77551940926c2a33af1112ee726",
+    "url": "/react-chess/static/js/2.c4f5bc72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3109b8b170613c2b150",
+    "url": "/react-chess/static/js/main.ef6cf7d1.chunk.js"
+  },
+  {
+    "revision": "7bf79dabeab6448c2b67",
+    "url": "/react-chess/static/js/runtime-main.0cd08013.js"
+  },
+  {
+    "revision": "a008b7dd1122cc7fc13c34e7684ff61f",
+    "url": "/react-chess/static/media/Screen Shot 2022-04-12 at 1.27.10 PM.a008b7dd.png"
+  },
+  {
+    "revision": "4fcaf27e5fe48103f4703bffdcd4f7ef",
+    "url": "/react-chess/static/media/Screen Shot 2022-04-12 at 1.27.33 PM.4fcaf27e.png"
+  },
+  {
+    "revision": "3382a95341b72c229183dcc0f1168176",
+    "url": "/react-chess/static/media/q_w.3382a953.png"
+  }
+]);
